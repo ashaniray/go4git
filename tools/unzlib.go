ba152603f1bad58fdbdf5fb1bbb036c3a691eb8c
Ashani Ray <ashaniray@gmail.com>
2016-02-03 12:19:28 +0530

    Updated unzlib

--- tools/unzlib.go
+++ tools/unzlib.go
@@ -6,12 +6,33 @@ import (
 	"os"
 )
 
+func getArgInputFile() (*os.File, error) {
+	args := os.Args[1:]
+	if len(args) > 0 {
+		return os.Open(args[0])
+	} else {
+		return os.Stdin, nil
+	}
+}
+
+func unzlib(in *os.File, out *os.File) error {
+	r, err := zlib.NewReader(in)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	_, err = io.Copy(out, r)
+	return err
+}
+
 func main() {
-	r, err := zlib.NewReader(os.Stdin)
+	f, err := getArgInputFile()
 	if err != nil {
 		panic(err)
 	}
-	io.Copy(os.Stdout, r)
-	r.Close()
+	err = unzlib(f, os.Stdout)
+	if err != nil {
+		panic(err)
+	}
 }
 
