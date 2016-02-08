86ee7fa5fe98a4e6a14a85d604e1f66d0cffad36
Anirban Gupta <agupta666@gmail.com>
2016-02-08 22:26:14 +0530

    ppcommit partial

--- tools/ppcommit.go
+++ tools/ppcommit.go
@@ -5,6 +5,10 @@ import(
 	"os"
 	"flag"
 	"io"
+	"bytes"
+	"strings"
+	"errors"
+	"strconv"
 )
 
 var repoRoot = flag.String("d", ".", "path to repo")
@@ -17,11 +21,64 @@ type Commit struct {
 	Committer string
 }
 
+func parseHeader(buff *bytes.Buffer) (int, error) {
+	header, err := buff.ReadString(0)
 
+	if err != nil {
+		return 0, err
+	}
+
+	xs := strings.Split(header, " ")
+	objType, objSize := xs[0], xs[1]
+
+	if objType != "commit" {
+		return 0, errors.New("not a commit object")
+	}
+
+	objSize = objSize[:len(objSize)-1] // remove trailing null
+
+	size, err := strconv.Atoi(objSize)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return size, nil
+}
+
+func parseBody(buff *bytes.Buffer, size int) (*Commit, error) {
+	body := string(buff.Next(size))
+
+	lines := strings.Split(body, "\n")
+
+	for _, l := range lines {
+		fmt.Println("LINE:", l)
+	}
+	
+
+	return nil, nil
+}
 
 func ParseCommit(in io.Reader) (*Commit, error) {
 
 	commit := new(Commit)
+
+	buff := new(bytes.Buffer)
+	buff.ReadFrom(in)
+
+	size, err := parseHeader(buff) 
+
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := parseBody(buff, size)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("BODY:", body)
 
 	return commit, nil
 }
