a7f45e9bb3b9404980457bc989ed7760d06eb835
Anirban Gupta <agupta666@gmail.com>
2016-02-11 17:22:11 +0530

    laa loose objects moved to repository

--- tools/repository.go
+++ tools/repository.go
@@ -3,6 +3,7 @@ package main
 import (
 	"path"
 	"path/filepath"
+	"io/ioutil"
 )
 
 type Repository struct {
@@ -40,4 +41,34 @@ func (r *Repository) LooseObjPath(sha string) string {
 	return filepath.Join(r.gitDir, "objects", sha[0:2], sha[2:])
 }
 
+func (r *Repository) LooseObjects() ([]string, error) {
 
+	objects := make([]string, 0)
+
+
+	objDir := filepath.Join(r.gitDir, "objects")
+
+	entries, err := ioutil.ReadDir(objDir)
+
+	if err != nil {
+		return objects, err
+	}
+
+	for _, e := range entries {
+		if e.IsDir() && len(e.Name()) == 2 {
+			subd := filepath.Join(objDir, e.Name())
+			subEntries, err := ioutil.ReadDir(subd)
+			if err != nil {
+				return objects, err
+			}
+
+			for _, se := range subEntries {
+				obj := e.Name() + se.Name()
+				objects = append(objects, obj)
+			}
+		}
+	}
+
+	return objects, nil
+
+}
