8d46e64e8d4b1282cb26d4e712e0481a07452f70
Ashani Ray <ashaniray@gmail.com>
2016-02-11 10:42:06 +0530

    updated reading index file

--- tools/packindex.go
+++ tools/packindex.go
@@ -35,40 +35,74 @@ func getTotalCount(in io.ReadSeeker) (uint, error) {
 	return uint(count), nil
 }
 
+func readHashAt(indexAt int, in io.ReadSeeker) (string, error) {
+	hashOffset := 0x408 + int64(indexAt)*0x14
+	_, err := in.Seek(hashOffset, 0)
+	if err != nil {
+		return "", err
+	}
+	hashBuff := make([]byte, 20)
+	_, err = in.Read(hashBuff)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(hashBuff), nil
+}
+
+func readCRCAt(indexAt int, count uint, in io.ReadSeeker) (string, error) {
+	crcOffset := 0x408 + int64(count)*0x14 + int64(indexAt)*4
+	_, err := in.Seek(crcOffset, 0)
+	if err != nil {
+		return "", err
+	}
+	crcBuff := make([]byte, 4)
+	_, err = in.Read(crcBuff)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(crcBuff), nil
+}
+
+func readOffsetAt(indexAt int, count uint, in io.ReadSeeker) (int, error) {
+	indexOffset := 0x408 + int64(count)*0x18 + int64(indexAt)*4
+	_, err := in.Seek(indexOffset, 0)
+	if err != nil {
+		return 0, err
+	}
+	indexBuff := make([]byte, 4)
+	_, err = in.Read(indexBuff)
+	if err != nil {
+		return 0, err
+	}
+	return int(binary.BigEndian.Uint32(indexBuff)), nil
+}
+
 func ReadPackIndexAt(indexAt int, in io.ReadSeeker) (PackIndex, error) {
 	count, err := getTotalCount(in)
 	if err != nil {
 		return PackIndex{}, err
 	}
-	hashOffset := 0x408 + int64(indexAt)*0x14
-	crcOffset := 0x408 + int64(count)*0x14 + int64(indexAt)*4
-	indexOffset := 0x408 + int64(count)*0x18 + int64(indexAt)*4
-
-	_, err = in.Seek(hashOffset, 0)
-	hashBuff := make([]byte, 20)
-	_, err = in.Read(hashBuff)
+	
+	hash, err := readHashAt(indexAt, in)
 	if err != nil {
 		return PackIndex{}, err
 	}
 
-	_, err = in.Seek(crcOffset, 0)
-	crcBuff := make([]byte, 4)
-	_, err = in.Read(crcBuff)
+	crc, err := readCRCAt(indexAt, count, in)
 	if err != nil {
 		return PackIndex{}, err
 	}
 
-	_, err = in.Seek(indexOffset, 0)
-	indexBuff := make([]byte, 4)
-	_, err = in.Read(indexBuff)
+	offset, err := readOffsetAt(indexAt, count, in)
 	if err != nil {
 		return PackIndex{}, err
 	}
 
-	return PackIndex{
-		hash:   hex.EncodeToString(hashBuff),
-		crc:    hex.EncodeToString(crcBuff),
-		offset: int(binary.BigEndian.Uint32(indexBuff))}, nil
+	return PackIndex {
+		hash:   hash,
+		crc:    crc,
+		offset: offset,
+	}, nil
 }
 
 func getFanoutValue(index int64, in io.ReadSeeker) (int, error) {
@@ -103,14 +137,14 @@ func GetObjectForHash(hash string, in io.ReadSeeker) (PackIndex, error) {
 
 	for start <= end {
 		curr := (start + end) / 2
-		indexObj, err := ReadPackIndexAt(curr, in)
+		hashOfObj, err := readHashAt(curr, in)
 		if err != nil {
 			return PackIndex{}, err
 		}
 		//switch (strings.Compare(hash, indexObj.hash[:len(hash)])) {
-		sliceHash := indexObj.hash[:len(hash)]
+		sliceHash := hashOfObj[:len(hash)]
 		if hash == sliceHash {
-			return indexObj, nil
+			return ReadPackIndexAt(curr, in)
 		}
 		if hash > sliceHash {
 			if start < curr {
