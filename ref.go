6f3a6b347b4b52c4643f5882089f281151fba5da
Anirban Gupta <agupta666@gmail.com>
2016-03-06 17:47:53 +0530

    added lsrefs

--- ref.go
+++ ref.go
@@ -1,7 +1,63 @@
 package go4git
 
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
+func refContent(path string) (string, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Trim(string(b), "\r\n"), nil
+}
+
 func (r *Repository) References() ([]Reference, error) {
-	return make([]Reference, 0), nil
+	refs := make([]Reference, 0)
+
+	headsDir := filepath.Join(r.gitDir, "refs", "heads")
+	tagsDir := filepath.Join(r.gitDir, "refs", "tags")
+
+	heads, err := ioutil.ReadDir(headsDir)
+
+	if err != nil {
+		return refs, err
+	}
+
+	for _, h := range heads {
+		n := filepath.Join("refs", "heads", h.Name())
+		rpath := filepath.Join(r.gitDir, n)
+
+		targetSha, err := refContent(rpath)
+		if err != nil {
+			return refs, err
+		}
+		ref := Reference{name: n, targetId: targetSha}
+		refs = append(refs, ref)
+	}
+
+	tags, err := ioutil.ReadDir(tagsDir)
+
+	if err != nil {
+		return refs, err
+	}
+
+	for _, t := range tags {
+		n := filepath.Join("refs", "tags", t.Name())
+		rpath := filepath.Join(r.gitDir, n)
+		targetSha, err := refContent(rpath)
+
+		if err != nil {
+			return refs, err
+		}
+		ref := Reference{name: n, targetId: targetSha}
+		refs = append(refs, ref)
+	}
+	return refs, nil
 }
 
 
@@ -27,6 +83,9 @@ func (r Reference) HasLog() bool {
 	return false
 }
 
+func (r Reference) String() string {
+	return fmt.Sprintf("%s %s", r.targetId, r.name)
+}
 
 func (r Reference) Type() string {
 	return ""
